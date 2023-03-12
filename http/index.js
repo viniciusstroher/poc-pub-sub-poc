65f87d7bf81543f751df042b853c8d0c4830f366
Vinicius Stroher <vinicius@socialdome.com>
2023-03-12 20:38:07 -0300

    ajuste

---
+++ http/index.js
@@ -0,0 +1,25 @@
+(async() => {
+  
+  const express = require('express')
+  const app = express()
+  const os = require("os")
+  const hostName = os.hostname()
+
+  const getDate = () => {
+    return new Date().toISOString()
+  }
+
+  app.get('/', (req, res) => {
+    console.log(`[${hostName}][${getDate()}] hit `)
+    return res.send({message: 'ok'})
+  })
+
+  app.get('/healthcheck', (req, res) => {
+    return res.send({message: 'ok'})
+  })
+  
+  const port = process.env.PORT || '3535'
+  
+  console.log(`[${hostName}][${getDate()}] start server at port ${port} `)
+  app.listen(port)
+})()
