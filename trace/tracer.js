8c7f70081aaea519530f49eef470f362ba4d6c03
Vinicius Stroher <vinicius@socialdome.com>
2023-06-26 00:36:56 -0300

    teste

---
+++ trace/tracer.js
@@ -0,0 +1,38 @@
+const process = require('process');
+const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { PeriodicExportingMetricReader, ConsoleMetricExporter } = require('@opentelemetry/sdk-metrics');
+const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
+
+const createTrace = (serviceName) => {
+  // configure the SDK to export telemetry data to the console
+  // enable all auto-instrumentations from the meta package
+  // const traceExporter = new ConsoleSpanExporter();
+  diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+
+  const sdk = new NodeSDK({
+    traceExporter: new OTLPTraceExporter(),
+    metricReader: new PeriodicExportingMetricReader({
+      exporter: new ConsoleMetricExporter()
+    }),
+    instrumentations: [getNodeAutoInstrumentations()]
+  });
+
+  // initialize the SDK and register with the OpenTelemetry API
+  // this enables the API to record telemetry
+  sdk.start()
+
+  // gracefully shut down the SDK on process exit
+  process.on('SIGTERM', () => {
+    sdk.shutdown()
+      .then(() => console.log('Tracing terminated'))
+      .catch((error) => console.log('Error terminating tracing', error))
+      .finally(() => process.exit(0));
+  });
+}
+
+module.exports = {
+  createTrace
+}
